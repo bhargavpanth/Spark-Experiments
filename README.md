e5504b62efec9d58d17334abcb1dcc5b4c28f45a
Bhargav Panth <bhargavrpanth@gmail.com>
2020-07-28 00:57:15 +0000

    trying out Spark on a local spark cluster

---
+++ README.md
@@ -0,0 +1,3 @@
+# Spark experiments
+
+Running a few experiments and learning Spark. README will be updated shortly
---
+++ download_dataset.py
@@ -0,0 +1,7 @@
+'''
+Downloading and extracting the MovieLense dataset
+'''
+import urllib3
+
+def download_dataset():
+    pass
