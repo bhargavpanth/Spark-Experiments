59e97bd7ab2f2469cd4f014a7972136955e9a516
Bhargav Panth <bhargavrpanth@gmail.com>
2020-07-29 21:34:52 +0000

    added a simple script to plot movie ratings

---
+++ ratings_counter.py
@@ -0,0 +1,28 @@
+from pyspark import SparkConf, SparkContext
+import collections
+
+class Ratings:
+    def __init__(self):
+        self.conf = SparkConf().setMaster('local').setAppName('ratings_histogram')
+        self.sc = SparkContext(conf = self.conf)
+        self.file = './ml-100k/u.data'
+
+    def load(self):
+        return self.sc.textFile(self.file)
+
+    def read_ratings(self):
+        lines = self.load()
+        ratings = lines.map(lambda x: x.split()[2])
+        return ratings.countByValue()
+
+    def plot(self):
+        result = self.read_ratings()
+        sortedResults = collections.OrderedDict(sorted(result.items()))
+        for key, value in sortedResults.items():
+            print('%s %i' % (key, value))
+
+def main():
+    ratings = Ratings.plot()
+
+if __name__ == "__main__":
+    main()
