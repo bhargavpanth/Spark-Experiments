0c9fe8b5dc514b5c566b49ec078ad39cb0ee6a2e
Bhargav Panth <bhargavrpanth@gmail.com>
2020-08-04 01:20:11 +0000

    WIP

--- filter_unwated_ratings.py
+++ filter_unwated_ratings.py
@@ -1,3 +1,10 @@
+from pyspark import SparkConf, SparkContext
+import collections
+
 class Filter:
     def __init__(self):
-        pass
+        self.conf = SparkConf().setMaster('local').setAppName('ratings_histogram')
+        self.sc = SparkContext(conf = self.conf)
+        self.file = './ml-100k/u.data'
+
+    
