a84788f7a59d09e04acb98ac94dbb357dc8dac9a
Bhargav Panth <bhargavrpanth@gmail.com>
2020-08-07 01:00:53 +0000

    WIP - adding word count class

--- word_count.py
+++ word_count.py
@@ -1,3 +1,8 @@
+from pyspark import SparkConf, SparkContext
+import collections
+
 class WordCount:
     def __init__(self):
-        pass
+        self.conf = SparkConf().setMaster('local').setAppName('ratings_histogram')
+        self.sc = SparkContext(conf = self.conf)
+        self.file = './datasets/modern_prometheus.txt'
