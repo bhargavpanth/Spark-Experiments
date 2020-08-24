8880a813e56b9f14103661345307ae6a3df5e2f6
Bhargav Panth <bhargavrpanth@gmail.com>
2020-08-24 00:31:45 +0000

    WIP

--- spark-sql.py
+++ spark-sql.py
@@ -1,18 +1,21 @@
 from pyspark.sql import SparkSession
 from pyspark.sql import Row
 
-spark = SparkSession.builder.appName('test_spark_sql').getOrCreate()
-
 def mapper(line):
     fields = line.split(',')
-    return Row(ID=int(fields[0]), name=str(fields[1].encode("utf-8")), age=int(fields[2]), numFriends=int(fields[3]))
+    return Row(ID=int(fields[0]), name=str(fields[1].encode('utf-8')), age=int(fields[2]), numFriends=int(fields[3]))
+
+class Session:
+    def __init__(self):
+        self.spark = SparkSession.builder.appName('test_spark_sql').getOrCreate()
+        self.file = './datasets/fakefriends.csv'
 
 lines = spark.sparkContext.textFile('./datasets/fakefriends.csv')
 people = lines.map(mapper)
 
 # Infer the schema, and register the DataFrame as a table.
-schemaPeople = spark.createDataFrame(people).cache()
-schemaPeople.createOrReplaceTempView('people')
+schema = spark.createDataFrame(people).cache()
+schema.createOrReplaceTempView('people')
 
 def get_people_of_different_ages():
     pre_teens = spark.sql('SELECT * FROM people WHERE age >= 10 AND age < 13')
@@ -21,6 +24,6 @@ def get_people_of_different_ages():
     adults = spark.sql('SELECT * FROM people WHERE age > 24')
     return [pre_teens, teenagers, young_adults, adults]
 
-schemaPeople.groupBy('age').count().orderBy('age').show()
+schema.groupBy('age').count().orderBy('age').show()
 
 spark.stop()
