d60ab6b22f685a78677f3f12d9e3670bae86621c
Bhargav Panth <bhargavrpanth@gmail.com>
2020-08-26 00:48:13 +0000

    WIP - cleaning up some dead comments

--- spark-sql.py
+++ spark-sql.py
@@ -5,21 +5,6 @@ def mapper(line):
     fields = line.split(',')
     return Row(ID=int(fields[0]), name=str(fields[1].encode('utf-8')), age=int(fields[2]), numFriends=int(fields[3]))
 
-# class Session:
-#     def __init__(self):
-#         self.spark = SparkSession.builder.appName('test_spark_sql').getOrCreate()
-#         self.file = './datasets/fakefriends.csv'
-
-#     def load(self):
-#         return self.spark.sparkContext.textFile(self.file)
-        
-#     def get_people_of_different_ages(self):
-#         pre_teens = self.spark.sql('SELECT * FROM people WHERE age >= 10 AND age < 13')
-#         teenagers = self.spark.sql('SELECT * FROM people WHERE age >= 13 AND age <= 19')
-#         young_adults = self.spark.sql('SELECT * FROM people WHERE age >= 19 AND age <= 23')
-#         adults = self.spark.sql('SELECT * FROM people WHERE age > 24')
-#         return [pre_teens, teenagers, young_adults, adults]
-
 spark = SparkSession.builder.appName('test_sparkql').getOrCreate()
 
 lines = spark.sparkContext.textFile('./datasets/fakefriends.csv')
