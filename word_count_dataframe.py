e5f0254292790d610f1f58e680bbfebceb6cbf78
Bhargav Panth <bhargavrpanth@gmail.com>
2020-08-29 01:51:46 +0000

    adding word count example - work in progress

--- word_count_dataframe.py
+++ word_count_dataframe.py
@@ -5,3 +5,7 @@ class WordCount:
     def __init__(self):
         self.spark = SparkSession.builder.appName('WordCount').getOrCreate()
         self.inputDF = self.spark.read.text('./datasets/modern_prometheus.txt')
+
+    def split(self):
+        words = self.inputDF.select(func.explode(func.split(self.inputDF.value, "\\W+")).alias("word"))
+        return words.filter(words.word != "")
