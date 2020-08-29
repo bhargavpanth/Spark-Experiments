f559ff44ee83cd56d9d4a76c41839872561cc69a
Bhargav Panth <bhargavrpanth@gmail.com>
2020-08-29 00:43:15 +0000

    adding word count example

--- word_count_dataframe.py
+++ word_count_dataframe.py
@@ -3,4 +3,5 @@ from pyspark.sql import functions as func
 
 class WordCount:
     def __init__(self):
-        pass
+        self.spark = SparkSession.builder.appName('WordCount').getOrCreate()
+        self.inputDF = self.spark.read.text('./datasets/modern_prometheus.txt')
