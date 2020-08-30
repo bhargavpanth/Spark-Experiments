99cadb0200064a17cdd213fc8b2f7aa506373064
Bhargav Panth <bhargavrpanth@gmail.com>
2020-08-30 00:18:46 +0000

    word count dataframe

--- word_count_dataframe.py
+++ word_count_dataframe.py
@@ -8,4 +8,16 @@ class WordCount:
 
     def split(self):
         words = self.inputDF.select(func.explode(func.split(self.inputDF.value, "\\W+")).alias("word"))
-        return words.filter(words.word != "")
+        return words.filter(words.word != '')
+
+    def normalize(self):
+        words = self.split()
+        return words.select(func.lower(words.word).alias('word'))
+
+    def sorted_word_count(self):
+        lowercaseWords = self.normalize()
+        wordCounts = lowercaseWords.groupBy('word').count()
+        # Sort by counts
+        wordCountsSorted = wordCounts.sort('count')
+        wordCountsSorted.show(wordCountsSorted.count())
+
