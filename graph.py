5cf3889edbabef9691b0e4b4348dc13f476e21dd
Bhargav Panth <bhargavrpanth@gmail.com>
2020-09-09 00:20:10 +0000

    unpacking too many values

--- graph.py
+++ graph.py
@@ -14,7 +14,7 @@ class Graph:
         return [names, lines]
 
     def connections(self):
-        [lines] = self.read()
+        [names, lines] = self.read()
         connections = lines.withColumn('id', func.split(func.col('value'), ' ')[0]) \
             .withColumn('connections', func.size(func.split(func.col('value'), ' ')) - 1) \
             .groupBy('id').agg(func.sum('connections').alias('connections'))
@@ -22,13 +22,16 @@ class Graph:
 
     def most_popular(self):
         connections = self.connections()
-        [names] = self.read()
+        [names, lines] = self.read()
         most_popular = connections.sort(func.col('connections').desc()).first()
         most_popular_name = names.filter(func.col('id') == most_popular[0]).select('name').first()
         print(most_popular_name[0] + ' is the most popular superhero with ' + str(most_popular[1]) + ' co-appearances.')
 
 def main():
     Graph().most_popular()
+    '''
+    CAPTAIN AMERICA is the most popular superhero with 1937 co-appearances.
+    '''
 
 if __name__ == '__main__':
     main()
