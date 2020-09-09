b57c641d34ccb0938c82d58f7087fcd5bf2e5b32
Bhargav Panth <bhargavrpanth@gmail.com>
2020-09-09 00:12:51 +0000

    added most popular method

--- graph.py
+++ graph.py
@@ -14,14 +14,21 @@ class Graph:
         return [names, lines]
 
     def connections(self):
-        [names, lines] = self.read()
+        [lines] = self.read()
         connections = lines.withColumn('id', func.split(func.col('value'), ' ')[0]) \
             .withColumn('connections', func.size(func.split(func.col('value'), ' ')) - 1) \
             .groupBy('id').agg(func.sum('connections').alias('connections'))
         return connections
 
+    def most_popular(self):
+        connections = self.connections()
+        [names] = self.read()
+        most_popular = connections.sort(func.col('connections').desc()).first()
+        most_popular_name = names.filter(func.col('id') == most_popular[0]).select('name').first()
+        print(most_popular_name[0] + ' is the most popular superhero with ' + str(most_popular[1]) + ' co-appearances.')
+
 def main():
-    pass
+    Graph().most_popular()
 
 if __name__ == '__main__':
     main()
