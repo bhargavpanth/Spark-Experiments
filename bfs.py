02a99acc8a3a3327dd730c5d027b1f51d1f69889
Bhargav Panth <bhargavrpanth@gmail.com>
2020-09-12 23:02:17 +0000

    WIP

--- bfs.py
+++ bfs.py
@@ -5,7 +5,22 @@ sc = SparkContext(conf = conf)
 
 class RDD:
     def __init__(self):
-        pass
+        self.text_file = sc.textFile('./dataset/graph.txt')
+        self.bfs = self.text_file.map(self.convert_to_bfs)
 
-    def convert_to_bfs(self):
-        pass
+    def convert_to_bfs(self, line):
+        start_id = 5306
+        fields = line.split()
+        person_id = int(fields[0])
+        connections = []
+        for connection in fields[1:]:
+            connections.append(int(connection))
+            
+        colour = 'WHITE'
+        distance = 9999
+        
+        if (person_id == start_id):
+            colour = 'GRAY'
+            distance = 0
+            
+        return (person_id, (connections, distance, colour))
