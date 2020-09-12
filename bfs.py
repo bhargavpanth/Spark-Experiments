ae88b44201d88bdc03b74c971e5aeeded0f05f8d
Bhargav Panth <bhargavrpanth@gmail.com>
2020-09-12 21:02:17 +0000

    WIP

--- bfs.py
+++ bfs.py
@@ -2,3 +2,10 @@ from pyspark import SparkConf, SparkContext
 
 conf = SparkConf().setMaster('local').setAppName('bfs_degree_of_seperation')
 sc = SparkContext(conf = conf)
+
+class RDD:
+    def __init__(self):
+        pass
+
+    def convert_to_bfs(self):
+        pass
