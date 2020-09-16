9f75845f070065f9c3c48eee34e9823a0c96b23f
Bhargav Panth <bhargavrpanth@gmail.com>
2020-09-16 01:24:52 +0000

    minor change

--- bfs.py
+++ bfs.py
@@ -5,30 +5,26 @@ sc = SparkContext(conf = conf)
 
 hit_counter = sc.accumulator(0)
 
-class RDD:
-    def __init__(self):
-        self.text_file = sc.textFile('./dataset/graph.txt')
-        self.bfs = self.text_file.map(self.__convert_to_bfs)
+def create_rdd():
+    text_file = sc.textFile('./dataset/graph.txt')
+    return text_file.map(convert_to_bfs)
 
-    def __convert_to_bfs(self, line):
-        start_id = 5306
-        fields = line.split()
-        person_id = int(fields[0])
-        connections = []
-        for connection in fields[1:]:
-            connections.append(int(connection))
-            
-        colour = 'WHITE'
-        distance = 9999
+def convert_to_bfs(line):
+    start_id = 5306
+    fields = line.split()
+    person_id = int(fields[0])
+    connections = []
+    for connection in fields[1:]:
+        connections.append(int(connection))
         
-        if (person_id == start_id):
-            colour = 'GRAY'
-            distance = 0
+    colour = 'WHITE'
+    distance = 9999
+    
+    if (person_id == start_id):
+        colour = 'GRAY'
+        distance = 0
 
-        return (person_id, (connections, distance, colour))
-
-    def convert_to_bfs(self):
-        return self.convert_to_bfs()
+    return (person_id, (connections, distance, colour))
 
 def bfs_map(node):
     target_id = 14
@@ -95,7 +91,7 @@ def bfs_reduce(node_one, node_two):
     return (edges, distance, color)
 
 def main():
-    iteration_rdd = RDD().convert_to_bfs()
+    iteration_rdd = create_rdd()
     for iteration in range(0, 10):
         print('Running BFS iteration# ' + str(iteration+1))
         mapped = iteration_rdd.flatMap(bfs_map)
