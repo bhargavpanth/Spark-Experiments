66ef10d28a5b7c8962ff509c244ea83cb07bf42c
Bhargav Panth <bhargavrpanth@gmail.com>
2020-09-23 00:28:12 +0000

    WIP

--- movie_similarity.py
+++ movie_similarity.py
@@ -5,3 +5,10 @@ from pyspark.sql import SparkSession
 from pyspark.sql.types import StructType, StructField, StringType, IntegerType, LongType
 
 spark = SparkSession.builder.appName('movie_similarities').master('local[*]').getOrCreate()
+
+
+def movie_name_schema():
+    return StructType([ StructField('movieID', IntegerType(), True), StructField('movieTitle', StringType(), True) ])
+
+def movie_schema():
+    pass
