8ea33f50d6e1f287f50d45b9452db5f58e48e6c6
Bhargav Panth <bhargavrpanth@gmail.com>
2020-09-23 00:13:17 +0000

    WIP

--- movie_similarity.py
+++ movie_similarity.py
@@ -1,3 +1,7 @@
 '''
 Script uses Spark's cluster manager
 '''
+from pyspark.sql import SparkSession
+from pyspark.sql.types import StructType, StructField, StringType, IntegerType, LongType
+
+spark = SparkSession.builder.appName('movie_similarities').master('local[*]').getOrCreate()
