8d80949cef9fbcfabf3cf80e3b28d36f55b4dbac
Bhargav Panth <bhargavrpanth@gmail.com>
2020-10-01 00:56:51 +0000

    WIP

--- linear-regression.py
+++ linear-regression.py
@@ -1,6 +1,9 @@
 from pyspark.sql import SparkSession
 
 spark = SparkSession.builder.appName('linear_regression').getOrCreate()
+lines = spark.sparkContext.textFile('./datasets/regression.txt')
+data = lines.map(lambda x: x.split(',')).map(lambda x: (float(x[0]), Vectors.dense(float(x[1]))))
+
 
 def main():
     pass
